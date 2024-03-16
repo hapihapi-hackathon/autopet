72690eb3e9f107952dd3d6ceabe20d359bdd389f
hanaosan <hanaosan0318@gmail.com>
2024-03-16 13:38:09 +0900

    commitNumberによって画像を切替

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<template>
+    <header>
+        <p>outpet</p>
+        <nav>
+            <ul>
+                <li><a href="/">setting</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header {
+  background-color: #CEBEB9;
+}
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import {computed, ref} from 'vue';
+import imageUrl1 from '@/assets/images/01.png';
+import imageUrl2 from '@/assets/images/02.png';
+import imageUrl3 from '@/assets/images/03.png';
+import imageUrl4 from '@/assets/images/04.png';
+import imageUrl5 from '@/assets/images/05.png';
+
+const username = ref('モナ'); // ダミーデータ
+const commitNumber = ref<number>(111); // ダミーデータ
+
+const petImage = computed<string>(() => {
+    if (commitNumber.value <= 25) {
+        return imageUrl1;
+    } else if (commitNumber.value <= 50) {
+        return imageUrl2;
+    } else if (commitNumber.value <= 75) {
+        return imageUrl3;
+    } else if (commitNumber.value <= 100) {
+        return imageUrl4;
+    } else {
+        return imageUrl5;
+    }
+});
+</script>
+
+<template>
+    <main class="main-content">
+      <h1>{{ username }}</h1>
+      <img :src="petImage" alt="Pet Image" class="pet-image"/>
+    </main>
+</template>
+
+<style scoped>
+main {
+  background-color: #CEBEB9;
+}
+</style>
