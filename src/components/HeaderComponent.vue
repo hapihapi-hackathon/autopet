81d0bb20f0d2c6f926a4324c1a5fbeddc238db32
Maho Miyata <mahomiyata@users.noreply.github.com>
2024-03-17 12:39:23 +0900

    ヘッダー調整

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,13 @@
+<script setup lang="ts">
+  import logo from '@/assets/images/logo.png';
+</script>
+
+
 <template>
     <nav class="navbar navbar-default">
       <div class="container-fluid">
         <div class="navbar-header">
-          <a class="navbar-brand" href="#">アウトペット</a>
+          <a class="navbar-brand" href="/"><img :src="logo" alt="outpet"/></a>
         </div>
         <div>
           <a class="navbar-text navbar-right" href="#">setting</a>
@@ -14,5 +19,12 @@
 <style scoped>
 nav {
   background-color: #CEBEB9;
+}
+.navbar-brand img {
+  width: 70%;
+}
+.navbar-text {
+  color: #212529;
+  font-size: 1.4rem;
 }
 </style>
